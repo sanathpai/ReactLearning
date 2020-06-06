8a9d9ac4cfe1e6ac3354c88d2df9215481ac02e9
sanath r pai <4nm17cs158@nmamit.in>
2020-06-06 12:49:26 +0530

    jsx configured

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt-me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "dog",
-      breed: "havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "bird",
-      breed: "cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "cat", breed: "Mixed" }),
-  ]);
+  // return React.createElement("div", { id: "something-important" }, [
+  //   React.createElement("h1", {}, "Adopt-me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "dog",
+  //     breed: "havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "bird",
+  //     breed: "cockatiel",
+  //   }),
+  //   React.createElement(Pet, { name: "Doink", animal: "cat", breed: "Mixed" }),
+  // ]);
+  //below usage of jsx
+  return (
+    <div>
+      <h1 id="something-important">Adopt-me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,17 @@
 import React from "react";
 //your code is going to go here.For now
 export default function ({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, name),
+  //     React.createElement("h2", {}, animal),
+  //     React.createElement("h2", {}, breed),
+  //   ]);
+  //this below is jsx
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
