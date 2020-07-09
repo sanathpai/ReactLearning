b0846fb17ded14205c3ec2c9019b2bf89c16c99b
sanath r pai <4nm17cs158@nmamit.in>
2020-07-09 19:51:51 +0530

    pet  component changed

--- src/Pet.js
+++ src/Pet.js
@@ -1,17 +1,19 @@
 import React from "react";
 //your code is going to go here.For now
-export default function ({ name, animal, breed }) {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, name),
-  //     React.createElement("h2", {}, animal),
-  //     React.createElement("h2", {}, breed),
-  //   ]);
-  //this below is jsx
+export default function ({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 }
